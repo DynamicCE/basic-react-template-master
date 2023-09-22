fce0f0b31a357ddab604d633eabc8297fb2c3f9f
Adınız <erkanmurat0632@gmail.com>
2023-09-22 17:25:48 +0300

    profile-card updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import React from "react";
+import "./App.css"; // CSS dosyasını import ediyoruz
+import catImage from "./cat.jpeg"; // Resmi import ediyoruz
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="card-container">
+      {/* Ana konteyner */}
+      <img src={catImage} alt="img" className="img" />
+      <div className="card-content">
+        {/* İçerik */}
+        Bu bir kart örneğidir.
+        <br />
+        <button className="udemy">Udemy</button> {/* Udemy butonu */}
+        <button className="youtube">YouTube</button> {/* YouTube butonu */}
+      </div>
+      <div className="card-footer">
+        {/* Alt kısım */}
+        Daha Fazla
+      </div>
+    </div>
+  );
 }
 
 export default App;
